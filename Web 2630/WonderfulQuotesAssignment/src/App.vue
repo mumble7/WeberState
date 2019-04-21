ead749521bff06b96249d14b1bd77bdac5547b9e
Matthew Young <36174887+mumble7@users.noreply.github.com>
2019-04-20 22:01:51 -0600

    Add files via upload

---
+++ Web 2630/WonderfulQuotesAssignment/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <!--ToDo: Use the Header component selector-->
+    <!--Bind data props quoteCount and set it equal to the quote length-->
+    <!--Bind data props maxQuotes and set it equal to the maxQuotes created below-->
+    <appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"></appHeader>
+    <!--ToDo: Use the NewQuote component selector-->
+    <!--Initiate an $emit event listener called quoteAdded that is equal to newQuote-->
+    <appNewQuote @quoteAdded="newQuote"></appNewQuote>
+    <!--ToDo: Use the QuoteGrid component selector-->
+    <!--Bind data props quotes and set it equal to the quotes array created below-->
+    <!--Call quoteDeleted that is emitted from the QuoteGrid Component and pass the deleteQuote method created below-->
+    <appQuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></appQuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//  Todo: Import QuoteGrid, NewQuote, and Header Components
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+export default {
+  data: function() {
+    return {
+      // ToDo: Create an array data object called quotes
+      // Have at least one string quote by default
+      // ToDo: Create a data object maxQuotes that is set to 10
+
+      quotes: [
+        "An unexamined life is not worth living.",
+        "An unexamined life is not worth living.",
+        "An unexamined life is not worth living.",
+        "An unexamined life is not worth living."
+      ],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    // ToDo: Create a method that checks when a newQuote is created and takes the parameter quote.
+    // This method will check if the quotes (Array Data Object) length is greater than or equal to the max quotes allowed
+    // If it is return an alert telling the user to delete a quote
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete some quotes first.");
+      }
+      // Outside of conditional, push quote (method parameter) to the quotes array
+      this.quotes.push(quote);
+    },
+
+    // ToDo: Create a method deleteQuote that takes in a parameter of index
+    // This method will splice the quotes array by (index, 1)
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    // ToDo: Initiate the QuoteGrid, NewQuote, and Header Components with Custom Selectors
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Web 2630/WonderfulQuotesAssignment/src/components/Quote.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <!--ToDo: Add default slot tag for passing the quote-->
+        <!-- right? -->
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+/*ToDo: Create a class panel that sets the font-family to preffered font*/
+/*Set a specific front size to your liking*/
+/*Set a color of your choice*/
+.panel-body {
+  font-family: "Open Sans";
+  font-size: 18px;
+  color: #237fbf;
+}
+/*ToDo: Create a class quote that sets the cursor to a pointer*/
+/*?*/
+.quote {
+  cursor: pointer;
+}
+/*ToDo: Create a hover effect on the class quote changing the background color to a color you prefer*/
+.quote:hover {
+  background-color: #75a6ac;
+}
+</style>
